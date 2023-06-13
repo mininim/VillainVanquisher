d4d9cf85cffaae93a40d6e4e4f83b90e12c41f13
mininim <minsoplee2015@gmail.com>
2023-06-13 11:34:59 +0900

    initial commit

---
+++ VillainVanquisher/VillainVanquisher/VillainVanquisherApp.swift
@@ -0,0 +1,17 @@
+//
+//  VillainVanquisherApp.swift
+//  VillainVanquisher
+//
+//  Created by Eric Lee on 2023/06/13.
+//
+
+import SwiftUI
+
+@main
+struct VillainVanquisherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
