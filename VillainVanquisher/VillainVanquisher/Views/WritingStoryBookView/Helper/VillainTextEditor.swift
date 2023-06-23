ef6763fd9c13e8c4421a8d2ff764028481065754
mininim <minsoplee2015@gmail.com>
2023-06-23 23:49:05 +0900

    [FEAT] VillainTextEditor 구현

---
+++ VillainVanquisher/VillainVanquisher/Views/WritingStoryBookView/Helper/VillainTextEditor.swift
@@ -0,0 +1,38 @@
+//
+//  VillainTextEidtor.swift
+//  VillainVanquisher
+//
+//  Created by Eric Lee on 2023/06/20.
+//
+
+import SwiftUI
+
+struct VillainTextEditor: View {
+    @State var text: String = ""
+    
+    var body: some View {
+        
+        VStack(spacing: 0){
+            
+            HStack(spacing: 0){
+                Text("특징")
+                    .font(.body)
+                    .foregroundColor(Color.primary)
+                Spacer()
+            }
+            
+            TextEditor(text: $text)
+                .frame(height: 250)
+            
+        }
+        
+        
+        
+    }
+}
+
+struct VillainTextEidtor_Previews: PreviewProvider {
+    static var previews: some View {
+        VillainTextEditor(text: "")
+    }
+}
