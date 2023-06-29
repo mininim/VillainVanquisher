291c986c5b18f8e31f0a8461e78d96f44170f6d2
mininim <minsoplee2015@gmail.com>
2023-06-29 22:36:03 +0900

    [FEAT] MakingStoryBookViewMdoel sendMessage 기능구현

--- VillainVanquisher/VillainVanquisher/ViewModels/MakingStoryBookViewModel.swift
+++ VillainVanquisher/VillainVanquisher/ViewModels/MakingStoryBookViewModel.swift
@@ -16,5 +16,47 @@ class MakingStoryBookViewModel: ObservableObject{
     @Published var content: String = ""
     @Published var coverImage: String = ""
     
+    @Published var isLoaded: Bool = false
+    
+    private let openAIService = OpenAIService()
+    
+    var messages: [Message] = []
+    
+    func sendMessage(){
+        
+        let nameAssistant = Message(id: UUID(), role: .assistant, content: "주인공을 괴롭히는 악당의 이름을 말해줘", createAt: Date())
+        let nameUserMessage = Message(id: UUID(), role: .user, content: self.villainName, createAt: Date())
+        
+        let secondAssistant = Message(id: UUID(), role: .assistant, content: "그 악당의 특징에 대해서 설명해줘", createAt: Date())
+        let characterMessage = Message(id: UUID(), role: .user, content: self.villainCharacter, createAt: Date())
+        
+        let defaultSystem = Message(id: UUID(), role: .system, content: "너는 악당의 이름과 특징을 기반으로 행복한 동화책을 작성하는 작가야", createAt: Date())
+        
+        messages = [nameAssistant, nameUserMessage, secondAssistant, characterMessage, defaultSystem]
+        
+        Task {
+            print(messages[0].content)
+            let response = await openAIService.sendMessage(messages: messages)
+            guard let receiveOpenAIMessage = response?.choices.first?.message else {
+                print("ERROR - No Received Message")
+                return
+            }
+            
+            await MainActor.run(body: {
+                self.content = receiveOpenAIMessage.content
+                self.isLoaded = true
+            })
+            
+        }
+        
+    }
+    
+}
+
+struct Message: Decodable{
+    let id: UUID
+    let role: SenderRole
+    let content: String
+    let createAt: Date
 }
 
